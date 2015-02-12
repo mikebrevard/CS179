dea05a6c08d29c93ccfc4700023ec754eb4c052e
Viet Pham <vpham013@ucr.edu>
2015-02-12 11:27:43 -0800

    Added paused menu

---
+++ PirateWars/Assets/Scripts/Paused.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var isPause = false;
+var MainMenu : Rect = Rect (465, 250, 200, 200);
+//MainMenu.center = new Vector2 (500, 500);
+function Update () 
+{
+	if( Input.GetKeyDown(KeyCode.Escape))
+	{
+		isPause = !isPause;
+		if(isPause)
+			Time.timeScale = 0;
+		else
+			Time.timeScale = 1;
+	}
+}
+function OnGUI()
+{
+	if(isPause)
+		GUI.Window(0, MainMenu, TheMainMenu, "Pause Menu");
+}
+
+function TheMainMenu () 
+{
+	if(GUILayout.Button("Main Menu"))
+	{
+		Application.LoadLevel("Game Start");
+	}
+	if(GUILayout.Button("Restart"))
+	{
+		isPause = false;
+		Time.timeScale = 1;
+		Application.LoadLevel("Game Main");
+	}
+	if(GUILayout.Button("Quit"))
+	{
+		Application.Quit();
+	}
+}
