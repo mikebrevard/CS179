bffa77982fe27ed3bc2cd9b0bf9bd572b258ca24
vpham013 <vpham013@ucr.edu>
2015-02-19 19:43:39 -0800

    Added water trail.

---
+++ PirateWars/Assets/Detonator Explosion Framework/Sample Supporting Emitters/Watertrail.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Watertrail : MonoBehaviour {
+
+	public Material firstMaterial;
+	public Material secondMaterial;
+	
+	private float startTime;
+	private float stopTime;
+	private float destroy;
+	
+	//the time at which this came into existence
+	private bool  isReallyOn;
+	
+	void Start (){
+		isReallyOn = particleEmitter.emit;
+		
+		//this kind of emitter should always start off
+		particleEmitter.emit = false;
+		
+		//get a random number between startTimeMin and Max
+		startTime = (0.0f + Time.time);
+		stopTime = (2.0f + Time.time);
+		destroy = (3.0f + Time.time);
+		
+		
+		//assign a random material
+		renderer.material = Random.value > 0.5f ? firstMaterial : secondMaterial;
+	}
+	
+	void FixedUpdate (){
+		//is the start time passed? turn emit on
+		if (Time.time > startTime)
+		{
+			particleEmitter.emit = true;
+		}
+		
+		if (Time.time > stopTime)
+		{
+			particleEmitter.emit = false;
+			if(Time.time > destroy)
+				Destroy (gameObject);
+		}
+	}
+}
--- PirateWars/Assets/Scripts/BoatController.cs
+++ PirateWars/Assets/Scripts/BoatController.cs
@@ -5,12 +5,19 @@ public class BoatController : MonoBehaviour
 {
 	public float speed;
 	public float rotationSpeed;
+	public GameObject water;
+	public GameObject water_left;
+	public GameObject water_right;
 	void Update()
 	{
 		if (Time.timeScale == 1)
 		{
-			rigidbody.AddRelativeForce (Vector3.forward * Input.GetAxis ("Vertical") * speed);
+			Vector3 speed_of_boat = Vector3.forward * Input.GetAxis ("Vertical") * speed;
+			rigidbody.AddRelativeForce (speed_of_boat);
 			transform.Rotate (Vector3.up * Input.GetAxis ("Horizontal") * rotationSpeed);
+				Instantiate(water, water_left.transform.position, water_left.transform.rotation);
+				Instantiate(water, water_right.transform.position, water_right.transform.rotation);
+
 		}
 	}
 }
