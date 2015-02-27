62f3105ce2d1a9b94fe86aa1ce7026dd2ac880b8
vpham013 <vpham013@ucr.edu>
2015-02-26 18:08:52 -0800

    Ship velocity for water trail

--- PirateWars/Assets/Scripts/BoatController.cs
+++ PirateWars/Assets/Scripts/BoatController.cs
@@ -15,9 +15,11 @@ public class BoatController : MonoBehaviour
 			Vector3 speed_of_boat = Vector3.forward * Input.GetAxis ("Vertical") * speed;
 			rigidbody.AddRelativeForce (speed_of_boat);
 			transform.Rotate (Vector3.up * Input.GetAxis ("Horizontal") * rotationSpeed);
+			if (rigidbody.velocity.magnitude > 8)
+			{
 				Instantiate(water, water_left.transform.position, water_left.transform.rotation);
 				Instantiate(water, water_right.transform.position, water_right.transform.rotation);
-
+			}
 		}
 	}
 }
