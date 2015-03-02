e5e82efac79e8b622423be85ccd483057ffaa5d5
nchou001 <nchou001@ucr.edu>
2015-03-02 13:30:10 -0800

    Added in weapon selection

---
+++ PirateWars/Assets/Scripts/CannonSelect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CannonSelect : MonoBehaviour {
+
+	public GameObject weapon1;
+	public GameObject weapon2;
+
+	// Use this for initialization
+	void Start () {
+		weapon2.SetActive (false);
+		weapon1.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("1")) 
+		{
+			weapon2.SetActive (false);
+			weapon1.SetActive (true);
+		}
+		if (Input.GetKeyDown ("2")) 
+		{
+			weapon1.SetActive (false);
+			weapon2.SetActive (true);
+		}
+
+	}
+}
---
+++ PirateWars/Assets/Shotgun.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Shotgun : MonoBehaviour {
+
+	public GameObject cannonball;
+	public GameObject flash;
+	public GameObject muzzle;
+	public float rateOfFire;
+	float fireDelay;
+	public float speed;
+	
+	void Update()
+	{
+		
+		if (Input.GetButton ("Fire1") && Time.time > fireDelay) 
+		{
+			GameObject ammoCounter = GameObject.FindGameObjectWithTag ("Ammo");
+			Ammo ammo = ammoCounter.GetComponent<Ammo>();
+			
+			if(ammo.getAmmo() > 0)
+			{
+				fireDelay = Time.time + rateOfFire;
+				GameObject clone = (GameObject)Instantiate (cannonball, transform.position, transform.rotation);
+				clone.rigidbody.velocity = transform.TransformDirection (new Vector3(0,0,speed));
+				Instantiate(flash, muzzle.transform.position, muzzle.transform.rotation);
+				
+				//Physics.IgnoreCollision (clone.collider, transform.root.collider);
+				audio.Play();
+				ammo.shotFired();
+			}
+		}
+	}
+}
