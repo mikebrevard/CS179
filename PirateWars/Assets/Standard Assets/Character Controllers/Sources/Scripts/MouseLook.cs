b98bd3a5907a0e10f350f3a5a505d6157cf6a041
vpham013 <vpham013@ucr.edu>
2015-03-09 15:10:09 -0700

    Fixed water to look more real, and added a whale

--- PirateWars/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ PirateWars/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -28,7 +28,7 @@ public class MouseLook : MonoBehaviour {
 	public float minimumY = -60F;
 	public float maximumY = 60F;
 
-	float rotationY = 0F;
+	public float rotationY = 0F;
 
 	void Update ()
 	{
@@ -57,7 +57,7 @@ public class MouseLook : MonoBehaviour {
 	void Start ()
 	{
 		// Make the rigid body not change rotation
-		if (GetComponent<Rigidbody>())
-			GetComponent<Rigidbody>().freezeRotation = true;
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
 	}
 }
---
+++ PirateWars/Assets/whale.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whale : MonoBehaviour {
+	private GameObject player;	
+	private GameObject ship;
+	public string tag;
+	
+	//speeds
+	public float rotationSpeed;
+	public float chaseSpeed;
+	public float patrolSpeed;
+	public float attackSpeed;
+	
+	//way point information
+	public Transform[] patrolWayPoints;				
+	private int wayPointIndex;								
+	private Transform wayPoint;
+	private float distanceToDestination;
+	
+	//state information
+	private string PATROL = "PATROL";
+	private string CHASE = "CHASE";
+	private string ATTACK = "ATTACK";
+	private string state;
+	
+	//attack information
+	private float firingRange = 150;
+	private float minumuDistance = 25;
+	private bool isNewAttackSequence;
+	private Vector3 attackDestination;
+
+	void Start () {
+		Patrolling ();
+	}
+	void Update () {
+		Patrolling ();
+	}
+	void Patrolling ()
+	{
+		if (distanceToDestination == 0) {
+			wayPointIndex++;
+			if (wayPointIndex == patrolWayPoints.Length) {
+				wayPointIndex = 0;
+			}
+			else {
+				wayPoint = patrolWayPoints[wayPointIndex];
+			}
+		}
+		
+		Move (transform.position, wayPoint.position, patrolSpeed);
+	}
+	
+	void Move(Vector3 current, Vector3 destination, float speed) {
+		if (destination - current != Vector3.zero) {
+			Quaternion rotation = Quaternion.LookRotation(destination - current);
+			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+		}
+		
+		distanceToDestination = Vector3.Distance (current, destination);
+		transform.position = Vector3.MoveTowards(current, destination, speed * Time.deltaTime);
+		//print ("current: " + current + "\t" + destination);
+	}
+}
