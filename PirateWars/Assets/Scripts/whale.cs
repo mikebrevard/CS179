c8e8a09681de09a11c53eb5569725202ded9a172
vpham013 <vpham013@ucr.edu>
2015-03-10 23:00:23 -0700

    Touching whale does damage and bounce.

--- PirateWars/Assets/Scripts/whale.cs
+++ PirateWars/Assets/Scripts/whale.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class whale : MonoBehaviour {
-	private GameObject player;	
+	GameObject player;	
 	private GameObject ship;
 	public string tag;
 	
@@ -11,7 +11,7 @@ public class whale : MonoBehaviour {
 	public float chaseSpeed;
 	public float patrolSpeed;
 	public float attackSpeed;
-	
+
 	//way point information
 	public Transform[] patrolWayPoints;				
 	private int wayPointIndex;								
@@ -23,13 +23,14 @@ public class whale : MonoBehaviour {
 	private string CHASE = "CHASE";
 	private string ATTACK = "ATTACK";
 	private string state;
-	
+
 	//attack information
 	private float firingRange = 150;
 	private float minumuDistance = 25;
 	private bool isNewAttackSequence;
 	private Vector3 attackDestination;
-
+	PlayerHealth playerHealth;
+	public float bounce;
 	void Start () {
 		Patrolling ();
 	}
@@ -60,5 +61,26 @@ public class whale : MonoBehaviour {
 		distanceToDestination = Vector3.Distance (current, destination);
 		transform.position = Vector3.MoveTowards(current, destination, speed * Time.deltaTime);
 		//print ("current: " + current + "\t" + destination);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		//ignores ships
+		if (other.tag == "Player") {
+			//print ("Collision");
+			//player loses health, bounces off terrain
+			player = GameObject.FindGameObjectWithTag ("Player");
+			playerHealth = player.GetComponent <PlayerHealth> ();
+			
+			float velocity = other.collider.rigidbody.velocity.x * other.collider.rigidbody.velocity.x
+				+ other.collider.rigidbody.velocity.z * other.collider.rigidbody.velocity.z;
+			
+			if (velocity < 50) {
+				other.collider.rigidbody.velocity = new Vector3 (-other.collider.rigidbody.velocity.x * 2.5f, 0, -other.collider.rigidbody.velocity.z * 2.5f);
+				playerHealth.TakeDamage (5);
+			} else {
+				other.collider.rigidbody.velocity = new Vector3 ((-bounce) * other.collider.rigidbody.velocity.x, 0, (-bounce) * other.collider.rigidbody.velocity.z);
+				playerHealth.TakeDamage (10);
+			}
+		}
 	}
 }
