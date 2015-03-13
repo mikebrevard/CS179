9e69bc991375312f5274a32bd61adbb616e4796a
vpham013 <vpham013@ucr.edu>
2015-03-13 11:53:06 -0700

    Fixed menu, buy shop, added water trail, new music, and whale to demo 4.

---
+++ PirateWars/Assets/Scripts/Enemies/EnemyWaterTrail.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWaterTrail : MonoBehaviour {
+	public GameObject water;
+	public GameObject water_left;
+	public GameObject water_right;
+	void Update()
+	{
+		if (Time.timeScale == 1)
+		{
+				Instantiate(water, water_left.transform.position, water_left.transform.rotation);
+				Instantiate(water, water_right.transform.position, water_right.transform.rotation);
+		}
+	}
+}
--- PirateWars/Assets/Scripts/MenuScript.cs
+++ PirateWars/Assets/Scripts/MenuScript.cs
@@ -6,6 +6,14 @@ public class MenuScript : MonoBehaviour {
 	public void OnClickPlay(){
 		Application.LoadLevel("Game Main");
 	}
+	public void OnClickTutorial()
+	{
+		Application.LoadLevel("Tutorial");
+	}
+	public void OnClickQuit()
+	{
+		Application.Quit();
+	}
 	void OnMouseOver () 
 	{
 		Screen.showCursor = false;
